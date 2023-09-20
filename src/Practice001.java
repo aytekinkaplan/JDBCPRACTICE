0826906f5aa58bfc6aaad1ee2c979002bd3fd62f
AYTEKİN KAPLAN <aoniki2018@gmail.com>
2023-09-20 19:00:25 +0300

    Commitment

--- src/Practice001.java
+++ src/Practice001.java
@@ -7,10 +7,18 @@ public class Practice001 {
         String password = "1234";
         Connection connection = DriverManager.getConnection(url, user, password);
         Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery("SELECT * FROM talebeler");
-        while(resultSet.next()){
-            System.out.printf("%-6d   %-20.20s  %-8s %-6d\n",resultSet.getInt(1),resultSet.getString(2),
-                    resultSet.getString(3),resultSet.getInt(4));
+        ResultSet resultSet1 = statement.executeQuery("SELECT * FROM talebeler");
+        System.out.println("   ***   task01   ***   ");
+        while(resultSet1.next()){
+            System.out.printf("%-6d   %-20.20s  %-8s %-6d\n",resultSet1.getInt(1),resultSet1.getString(2),
+                    resultSet1.getString(3),resultSet1.getInt(4));
+        }
+
+        System.out.println("   ***   task02   ***   ");
+        ResultSet resultSet2 = statement.executeQuery("SELECT * FROM talebeler where yazili_notu");
+        while(resultSet2.next()){
+            System.out.printf("%-6d   %-20.20s  %-8s %-6d\n",resultSet2.getInt(1),resultSet2.getString(2),
+                    resultSet2.getString(3),resultSet2.getInt(4));
         }
     }
 }
